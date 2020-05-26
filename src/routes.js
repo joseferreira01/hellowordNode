9dae9aaaac457f6a10d277811d1748f79a417b1f
josé <joseifpb2015@gmail.com>
2020-05-26 10:53:34 -0300

    get

--- src/routes.js
+++ src/routes.js
@@ -9,4 +9,9 @@ routes.post('/ongs',(Request,Response)=> {
     const id = crypto.randomBytes(4).toString('Hex');
     return Response.json({name});
 });
+routes.get('/ongs',(Request,Response)=> {
+   const id = crypto.randomBytes(4).toString('Hex');
+   console.log(id);
+   return Response.json({id});
+});
    module.exports = routes;
